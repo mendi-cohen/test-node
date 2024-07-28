9a7339a8f0dcd60ddb74d94b28e9e3f0fc30a0bb
Your GitHub Username <your-email@example.com>
2024-07-28 16:33:36 +0300

    ניסוי 3

--- Routers/Router-Users.js
+++ Routers/Router-Users.js
@@ -3,6 +3,7 @@ import SignIn from '../Controllers/Controll-Users.js';
 const router = express.Router();
 
 router.post("/sign-in", SignIn.sign_in);
+router.post("/get", SignIn.Allusers);
 
 
 export default router;
