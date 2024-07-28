47c7f4ee31ea3c8517fc7eedbb1ba0a8f8e15a36
Your GitHub Username <your-email@example.com>
2024-07-28 15:58:18 +0300

    ניסוי ראשון להריץ בנטליפיי

--- functions/Server.js
+++ functions/Server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import users from './Routers/Router-Users.js';
+import users from '../Routers/Router-Users.js'; 
+import serverless from 'serverless-http';
 
 const app = express();
 dotenv.config();
@@ -9,12 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//Routers
-app.use('/users' ,users);
-
-
-// Port
-const port = process.env.PORT;
-app.listen(port, () => {
-    console.log(`running on port ${port}`);
-});
+// Routers
+app.use('/', users);
+// מודול אקספורט
+export const handler = serverless(app);
